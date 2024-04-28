3d5c7ad18328f2e6a7ba11be8a2f10ddb3b857d0
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-04-28 18:14:04 +0500

    age in the story updated

--- 03_variables/giraffe.rb
+++ 03_variables/giraffe.rb
@@ -1,4 +1,4 @@
 puts "There once was a man named John"
-puts "he was 70 years old."
+puts "he was 35 years old."
 puts "He really liked the name John"
-puts "but didn't like being 70."
+puts "but didn't like being 35."
