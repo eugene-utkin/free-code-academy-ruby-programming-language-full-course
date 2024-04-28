11168ef0c3ed85bd5beb3dc3a592814ba21fb297
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-04-28 18:13:31 +0500

    name in the story updated

--- 03_variables/giraffe.rb
+++ 03_variables/giraffe.rb
@@ -1,4 +1,4 @@
-puts "There once was a man named George"
+puts "There once was a man named John"
 puts "he was 70 years old."
-puts "He really liked the name George"
+puts "He really liked the name John"
 puts "but didn't like being 70."
