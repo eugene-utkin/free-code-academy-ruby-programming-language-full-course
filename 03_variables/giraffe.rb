d2cce576fe4148ffd98558fbf1d2afc95d563785
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-04-29 16:08:37 +0500

    character_name variable used again

--- 03_variables/giraffe.rb
+++ 03_variables/giraffe.rb
@@ -2,5 +2,5 @@ character_name = "John"
 character_age = "35"
 puts ("There once was a man named " + character_name)
 puts "he was 35 years old."
-puts "He really liked the name John"
+puts "He really liked the name " + character_name
 puts "but didn't like being 35."
