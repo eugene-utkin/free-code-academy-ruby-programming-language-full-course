85f922378e930cd05254b05c181010dcac2d7962
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-05-01 16:38:15 +0500

    character_name reassigned

--- 03_variables/giraffe.rb
+++ 03_variables/giraffe.rb
@@ -2,5 +2,6 @@ character_name = "Mike"
 character_age = "35"
 puts ("There once was a man named " + character_name)
 puts ("he was " + character_age + " years old.")
+character_name = "Tom"
 puts ("He really liked the name " + character_name)
 puts ("but didn't like being " + character_age + ".")
