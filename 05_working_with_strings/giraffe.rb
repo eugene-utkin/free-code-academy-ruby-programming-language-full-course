a5d021526c9f6ca09cd57a65d52e2a8dc2d2c55c
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-05-03 18:08:21 +0500

    phrase reassigned

--- 05_working_with_strings/giraffe.rb
+++ 05_working_with_strings/giraffe.rb
@@ -4,6 +4,9 @@ phrase = "Giraffe Academy"
 puts phrase
 puts phrase.upcase()
 puts phrase.downcase()
+
 phrase = "      Giraffe Academy      "
 puts phrase
 puts phrase.strip()
+
+phrase = "Giraffe Academy"
