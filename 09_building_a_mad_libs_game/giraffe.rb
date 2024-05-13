17436defd2ee85539e91f58be756fcfddd38cc16
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-05-13 18:08:11 +0500

    celebrity variable added

--- 09_building_a_mad_libs_game/giraffe.rb
+++ 09_building_a_mad_libs_game/giraffe.rb
@@ -3,6 +3,7 @@ color = gets.chomp()
 puts "Enter a plural noun: "
 plural_noun = gets.chomp()
 puts "Enter a celebrity: "
+celebrity = gets.chomp()
 
 puts ("Roses are {color}")
 puts ("{plural_noun} are blue")
