d0bb1f5690d440b3fbe9a5852747c5d7580d42a7
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-05-26 17:52:41 +0500

    puts used

--- 14_if_statements/giraffe.rb
+++ 14_if_statements/giraffe.rb
@@ -18,7 +18,7 @@ istall = true
 if ismale and istall
   puts "You are a tall male"
 elsif ismale and !istall
-
+  puts "You are a short male"
 else
   puts "You either not male or not tall or both"
 end
