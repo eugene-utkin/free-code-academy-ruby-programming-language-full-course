04e92bf3aacdf9a5ddec0207ad9de73fe9ceb9e6
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-13 22:18:38 +0500

    readlines example

--- 23_reading_files/giraffe.rb
+++ 23_reading_files/giraffe.rb
@@ -14,3 +14,8 @@ File.open("employees.txt", "r") do |file|
   puts file.readchar()
   puts file.readchar()
 end
+
+File.open("employees.txt", "r") do |file|
+  for line in file.readlines()
+  end
+end
