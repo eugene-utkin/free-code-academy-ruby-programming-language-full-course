f020bbbaf7dfdcd389c196184ed636a52d61d31b
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-16 19:43:59 +0500

    extra block removed

--- 25_handling_errors/giraffe.rb
+++ 25_handling_errors/giraffe.rb
@@ -2,13 +2,8 @@ lucky_nums = [4, 8, 15, 16, 23, 42]
 # lucky_nums["dog"]
 
 begin
+  lucky_nums["dog"]
   num = 10 / 0
 rescue
   puts "Division by zero error"
-end
-
-begin
-  lucky_nums["dog"]
-rescue
-
 end
