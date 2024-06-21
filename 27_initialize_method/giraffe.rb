d46adb235b8c02dd84ea8767c3e06deb4879da13
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-21 20:17:11 +0500

    author assignment removed

--- 27_initialize_method/giraffe.rb
+++ 27_initialize_method/giraffe.rb
@@ -13,7 +13,6 @@ book1 = Book.new("Harry Potter", "JK Rowling", 400)
 puts book1.pages
 
 book2 = Book.new("Lord of the rings", "Tolkien", 500)
-book2.author = "Tolkien"
 book2.pages = 500
 
 puts book2.author
