2cdfdf0ad547b4d323defb0aedf73bbd9c882b5a
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-26 16:29:35 +0500

    questions array created

--- 29_building_a_quiz/giraffe.rb
+++ 29_building_a_quiz/giraffe.rb
@@ -1,6 +1,5 @@
 class Question
   attr_accessor :prompt, :answer
-
   def initialize(prompt, answer)
     @prompt = prompt
     @answer = answer
@@ -10,3 +9,5 @@ end
 p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
 p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
 p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
+
+questions = []
