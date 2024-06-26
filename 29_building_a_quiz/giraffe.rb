32a94a7df02e6c5ff61158e8a1d33620e2527fa9
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-26 16:24:18 +0500

    Question class created

--- 29_building_a_quiz/giraffe.rb
+++ 29_building_a_quiz/giraffe.rb
@@ -1,3 +1,7 @@
+class Question
+
+end
+
 p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
 p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
 p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
