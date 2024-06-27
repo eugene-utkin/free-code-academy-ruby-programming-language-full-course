f05bb5f0ca3d2d4c16b95d070ca6fd5b0daa2679
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-06-27 18:40:05 +0500

    new question created

--- 29_building_a_quiz/giraffe.rb
+++ 29_building_a_quiz/giraffe.rb
@@ -10,4 +10,6 @@ p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
 p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
 p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
 
-questions = []
+questions = [
+  Question.new()
+]
